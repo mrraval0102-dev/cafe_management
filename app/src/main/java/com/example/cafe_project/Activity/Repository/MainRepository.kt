b9fc42cefed788afb0df553c1d2679758dedc1d1
Sanket Khode <sanketkhode85749@gmail.com>
2025-07-26 13:20:00 +0530

    (fix): firebase dependency changes

--- app/src/main/java/com/example/cafe_project/Activity/Repository/MainRepository.kt
+++ app/src/main/java/com/example/cafe_project/Activity/Repository/MainRepository.kt
@@ -1,6 +1,8 @@
 package com.example.cafe_project.Activity.Repository
 
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class MainRepository {
-    private val firebaseDatabse = Firebase
 }
