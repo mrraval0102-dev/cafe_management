4c723cd47a24ba56d31a1898e1c312b8c2476c60
mrraval0102-dev <mr.raval0102@gmail.com>
2025-07-27 22:13:19 +0530

    items ui done

---
+++ app/src/main/java/com/example/cafe_project/Activity/DetailActivity.kt
@@ -0,0 +1,90 @@
+package com.example.cafe_project.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+import com.bumptech.glide.Glide
+import com.example.cafe_project.Activity.Domain.ItemsModel
+import com.example.cafe_project.R
+import com.example.cafe_project.databinding.ActivityDetailBinding
+import com.example.project1762.Helper.ManagmentCart
+
+class DetailActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailBinding
+    private lateinit var item: ItemsModel
+    private lateinit var managementCart : ManagmentCart
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        managementCart = ManagmentCart(this)
+
+        bundle()
+        initSizeList()
+
+    }
+
+    private fun initSizeList() {
+        binding.apply {
+            smallBtn.setOnClickListener {
+                smallBtn.setBackgroundResource(R.drawable.stroke_brown_bg)
+                mediumBtn.setBackgroundResource(0)
+                largeBtn.setBackgroundResource(0)
+            }
+            mediumBtn.setOnClickListener {
+                smallBtn.setBackgroundResource(0)
+                mediumBtn.setBackgroundResource(R.drawable.stroke_brown_bg)
+                largeBtn.setBackgroundResource(0)
+            }
+            largeBtn.setOnClickListener {
+                smallBtn.setBackgroundResource(0)
+                mediumBtn.setBackgroundResource(0)
+                largeBtn.setBackgroundResource(R.drawable.stroke_brown_bg)
+            }
+        }
+    }
+
+    private fun bundle() {
+        binding.apply {
+            item = intent.getSerializableExtra("object") as ItemsModel
+
+            Glide.with(this@DetailActivity).load(item.picUrl[0]).into(binding.picMain)
+
+            titleTxt.text = item.title
+            descriptionTxt.text = item.description
+            priceTxt.text = "$"+item.price
+            ratingTxt.text = item.rating.toString()
+
+            addToCartBtn.setOnClickListener {
+                item.numberInCart = Integer.valueOf(
+                    numberItemTxt.text.toString()
+                )
+                managementCart.insertItems(item)
+            }
+
+            backBtn.setOnClickListener {
+                startActivity(Intent(this@DetailActivity, MainActivity::class.java))
+            }
+
+            plusCart.setOnClickListener {
+                numberItemTxt.text = (item.numberInCart + 1 ).toString()
+                item.numberInCart++
+            }
+
+            minusBtn.setOnClickListener {
+                if(item.numberInCart > 1){
+                    numberItemTxt.text = (item.numberInCart - 1).toString()
+                    item.numberInCart--
+                }
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/example/cafe_project/Adapter/PopularAdapter.kt
+++ app/src/main/java/com/example/cafe_project/Adapter/PopularAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.cafe_project.Adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.cafe_project.Activity.DetailActivity
 import com.example.cafe_project.Activity.Domain.ItemsModel
 import com.example.cafe_project.databinding.ViewholderPopularBinding
 
@@ -24,6 +26,13 @@ class PopularAdapter(val items:MutableList<ItemsModel>): RecyclerView.Adapter<Po
         holder.binding.priceTxt.text = "$" + items[position].price.toString()
 
         Glide.with(context).load(items[position].picUrl[0]).into(holder.binding.pic)
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context,DetailActivity::class.java)
+            intent.putExtra("object",items[position])
+            context.startActivity(intent)
+        }
+
     }
 
     override fun getItemCount(): Int = items.size
